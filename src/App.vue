6aea3542d60f6f061360ec076004ae15471793c3
Zhalo6 <99208362+Zhalo6@users.noreply.github.com>
2024-05-06 15:14:25 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+const count = ref(0)
+const awesome = ref(true)
+
+function onInput(e) {
+  text.value = e.target.value
+}
+
+function increment() {
+  count.value++
+}
+
+function toggle() {
+  awesome.value = !awesome.value
+}
+  
+</script>
+
+<template>
+  <div>
+    <button @click="toggle">Toggle</button>
+  </div>
+  
+  <div v-show="awesome">
+
+  <input :value="text" @input="onInput" placeholder="Type here">
+  <p>{{ text }}</p>
+  <button @click="increment">Count is: {{ count }}</button>
+
+  </div>
+</template>
