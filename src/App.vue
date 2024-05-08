59239bb46ca946facfd3bc68ca8fbaeb5c615ac4
Zhalo6 <99208362+Zhalo6@users.noreply.github.com>
2024-05-08 15:31:08 +0000

    Added basic styling, it doesn't work properly, added 3 buttons with no functions. Started working on actual design and making of the example from Figma.

--- src/App.vue
+++ src/App.vue
@@ -9,26 +9,75 @@ function onInput(e) {
   text.value = e.target.value
 }
 
-function increment() {
-  count.value++
-}
-
 function toggle() {
   awesome.value = !awesome.value
 }
   
 </script>
 
+<style>
+  .background {
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    width: 1920px;
+    height: 1080px;
+    background: #102531;
+  }
+  .center {
+    position: absolute;
+    top: 150%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
+    background: #09202D;
+    box-shadow: 0px 4px 8px 3px rgba(0, 0, 0, 0.15), 0px 1px 3px rgba(0, 0, 0, 0.3);
+    border-radius: 4px;
+  }
+  .buttonOne {
+    position: absolute;
+    top: 250%;
+    left: 25%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
+    background: #09202D;
+    box-shadow: 0px 7px 21px rgba(0, 0, 0, 0.2);
+    border-radius: 6px;
+  }
+  .buttonTwo {
+    position: absolute;
+    top: 250%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
+    background: #09202D;
+    box-shadow: 0px 7px 21px rgba(0, 0, 0, 0.2);
+    border-radius: 6px;
+  }
+  .buttonThree {
+    position: absolute;
+    top: 250%;
+    left: 75%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
+    background: #09202D;
+    box-shadow: 0px 7px 21px rgba(0, 0, 0, 0.2);
+    border-radius: 6px;
+  }
+</style>
+
 <template>
-  <div>
-    <button @click="toggle">Toggle</button>
+  <div class="background"></div>
+  <div class="center">
+    <input :value="text" @input="onInput" placeholder="Enter Champion...">
   </div>
-  
-  <div v-show="awesome">
-
-  <input :value="text" @input="onInput" placeholder="Type here">
-  <p>{{ text }}</p>
-  <button @click="increment">Count is: {{ count }}</button>
-
+  <div class="buttonOne">
+    <button>Team Comps</button>
+  </div>
+  <div class="buttonTwo">
+    <button>Best In Slot</button>
+  </div>
+  <div class="buttonThree">
+    <button>Augments</button>
   </div>
 </template>
